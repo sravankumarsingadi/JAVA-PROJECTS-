825831689d84287d5e10c00ff8912aa10e0a87ff
sravankumarsingadi <97672716+sravankumarsingadi@users.noreply.github.com>
2022-03-02 13:42:06 +0530

    Delete Phase3 Project/SportyShoes directory

--- Phase3 Project/SportyShoes/src/main/java/com/project/SportyShoes/jdbc/CategoriesRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.SportyShoes.jdbc;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.SportyShoes.entity.Categories;
-
-public interface CategoriesRepository extends JpaRepository<Categories, Long> {
-
-}
